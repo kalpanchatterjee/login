01f2021142da04614e994b93a94ab41ad3824045
kalpan.chattopadhyay <kalpan.chattopadhyay@rakuten.com>
2022-08-09 13:23:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
+import Cookies from "js-cookie";
+const apiUrl = 'http://localhost:3001';
+axios.interceptors.request.use(
+  config => {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [apiUrl];
+    const token = localStorage.getItem('token');
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  useEffect(()=>{
+    const {data }=  axios.get(`${apiUrl}/getcookie`);
+   
+    
+  },[])
+  const storedJwt = localStorage.getItem('token');
+  const [jwt, setJwt] = useState(storedJwt || null);
+  const [foods, setFoods] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+const getJwt = async () => {
+    const { data } = await axios.get(`${apiUrl}/jwt`,{ withCredentials: true });
+    //  window.location.replace("http://localhost:3000?token="+data.token);
+     localStorage.setItem('token', data.token);
+     //Cookies.set('token',data.token);
+     document.cookie=data.token;
+    // Cookies.set('token',data.token);
+     setJwt(data.token);
+
+  };
+
+const getFoods = async () => {
+    try {
+      const { data } = await axios.get(`${apiUrl}/foods`,{ withCredentials: true });
+     // const {data1 }=  axios.get(`${apiUrl}/getcookie`,{ withCredentials: true });
+      setFoods(data);
+      setFetchError(null);
+    } catch (err) {
+      setFetchError(err.message);
+    }
+  };
+return (
+    <>
+      <section style={{ marginBottom: '10px' }}>
+        <button onClick={() => getJwt()}>Get JWT</button>
+        {jwt && (
+          <pre>
+            <code>{jwt}</code>
+          </pre>
+        )}
+      </section>
+      <section>
+        <button onClick={() => getFoods()}>
+          Get Foods
+        </button>
+        <ul>
+          {foods.map((food, i) => (
+            <li>{food.description}</li>
+          ))}
+        </ul>
+        {fetchError && (
+          <p style={{ color: 'red' }}>{fetchError}</p>
+        )}
+      </section>
+    </>
   );
 }
-
 export default App;
---
+++ src/Cookie.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { useParams } from 'react-router-dom';
+import Cookies from 'js-cookie';
+const apiUrl = 'http://localhost:3001';
+
+function Cookie() {
+    let { id } = useParams();
+    useEffect(()=>{
+    //    return localStorage.getItem('token');
+    
+     sendGetRequest(id);
+    },[]);
+   
+   // let token=localStorage.getItem('token');
+    
+
+    // const calling  =await axios.get(`${apiUrl}/getcookie`,{ withCredentials: true }).then((data)=>{
+    //     // alert(data)
+    //      if(typeof(data)!="undefined" && data!=null){
+    //          console.log(data)
+    //          redirect(data);
+    //      }
+    //      })
+    const sendGetRequest = async (id) => {
+        try {
+            //const resp = await axios.get(`${apiUrl}/getcookie`,{ withCredentials: true });
+            console.log(document.cookie);
+            console.log(Cookies.get('token'));
+           // console.log("http://localhost:"+id+"?token="+resp.data.token)
+           // window.location.replace("http://localhost:"+id+"/"+resp.data.token);
+            //console.log(resp.data.token);
+        } catch (err) {
+            // Handle Error Here
+            console.error(err);
+        }
+    };
+    return <>Hi</>
+}
+export default Cookie;
